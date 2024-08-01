facf2694892185b2bb7a9e7a8f506cec7e5e829f
FabricioCCapone <fabricapone12@gmail.com>
2024-08-01 02:14:28 +0200

    Created basic components and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Import the components we created
 import Home from './pages/Home';
-import About from './pages/About';
-import Dashboard from './pages/Dashboard'; 
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className='pages'>
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='about' element={<About />} />
-            <Route path='dashboard' element={<Dashboard />} />
           </Routes>
         </div>
       </BrowserRouter>
