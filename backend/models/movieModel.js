87da8e966183f50c12def765f14fee2a17c29570
FabricioCCapone <fabricapone12@gmail.com>
2024-08-06 21:50:59 +0200

    Added Img, duration, review and genre to movieModel.js

--- backend/models/movieModel.js
+++ backend/models/movieModel.js
@@ -3,6 +3,31 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+// Define the allowed genres
+const allowedGenres = [
+    'Action',
+    'Comedy',
+    'Drama',
+    'Fantasy',
+    'Horror',
+    'Mystery',
+    'Romance',
+    'Thriller',
+    'Western',
+    'Science Fiction',
+    'Animation',
+    'Adventure',
+    'Crime',
+    'Documentary',
+    'Family',
+    'History',
+    'Music',
+    'War',
+    'TV Movie',
+    'Foreign',
+    'Other'
+];
+
 // Movie schema with id, title, director, and year fields
 const movieSchema = new Schema({
     title: {
@@ -17,10 +42,35 @@ const movieSchema = new Schema({
         type: Number,
         required: true
     },
+    genres: {
+        type: [String],
+        enum: allowedGenres,
+        required: true,
+        validate: {
+            validator: function (arr) {
+                return arr.length > 0; // Ensure there's at least one genre
+            },
+            message: 'At least one genre is required.'
+        }
+    },
+    duration: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 500,
+    },
+    review: {
+        type: String,
+        required: false
+    },
     rating: {
         type: Number,
         required: false
-    }
+    },
+    imageUrl: {
+        type: String,
+        required: true // or false if it's optional
+      }
 },
     {
         timestamps: true
--- frontend/src/components/MovieDetails.js
+++ frontend/src/components/MovieDetails.js
@@ -1,12 +1,19 @@
 const MovieDetails = ({ movie }) => {
     return (
-        <div className="">
-        <h3>{movie.name}</h3>
-        <p>Director: {movie.director}</p>
-        <p>Year of release: {movie.year}</p>
-        <p>Personal rating: {movie.rating}</p>
+        <div className="movie-details">
+            <h3>{movie.title}</h3>
+            <div>
+                <p><strong>Director: </strong>{movie.director}</p>
+                <p><strong>Year of Release: </strong>{movie.year}</p>
+                <p><strong>Personal Rating: </strong>{movie.rating}</p>
+            </div>
+            <div>
+                <p><strong>Genre: </strong>{movie.genre}</p>
+                <p><strong>Duration: </strong>{movie.duration} minutes</p>
+                <p><strong>My Review: </strong>{movie.review}</p>
+            </div>
         </div>
     );
-    }
+}
 
 export default MovieDetails;
